f3f3b01b93d31f9e2eaa4dd89de68bcc7334158b
Евгений <ulyanov.1992@list.ru>
2020-08-17 10:40:35 +0300

    Splash screen

---
+++ app/src/main/java/com/u/e/m/salary/ShebuleActivity.java
@@ -0,0 +1,14 @@
+package com.u.e.m.salary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShebuleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shebule);
+    }
+}

2f0a8d0e3a2c633e9943c25bd767deb0904b7c86
Евгений <ulyanov.1992@list.ru>
2020-08-18 12:39:45 +0300

    Реализован рассчет заработной платы по дневному графику.

--- app/src/main/java/com/u/e/m/salary/DayActivity.java
+++ app/src/main/java/com/u/e/m/salary/DayActivity.java
@@ -30,10 +30,10 @@ public class DayActivity extends AppCompatActivity {
     public void calculationDay(View view) {
 
         Intent intent = new Intent(DayActivity.this, TotalActivity.class);
-        int days = Integer.parseInt(dayWorkEditText.getEditText().getText().toString());
-        int daysMore = Integer.parseInt(dayMoreEditText.getEditText().getText().toString());
-        int daysAllovance = Integer.parseInt(allowanceEditText.getEditText().getText().toString());
-        int daysPrize = Integer.parseInt(prizeEditText.getEditText().getText().toString());
+        String days = dayWorkEditText.getEditText().getText().toString();
+        String daysMore = dayMoreEditText.getEditText().getText().toString();
+        String daysAllovance = allowanceEditText.getEditText().getText().toString();
+        String daysPrize = prizeEditText.getEditText().getText().toString();
         intent.putExtra("days", days);
         intent.putExtra("daysMore", daysMore);
         intent.putExtra("daysAllovance", daysAllovance);
@@ -41,21 +41,3 @@ public class DayActivity extends AppCompatActivity {
         startActivity(intent);
     }
 }
-
-//        txtView.setText("Оклад " + (hours*stavka)  +
-//        "\nПремия " + (hours*stavka)*0.3f +
-//        "\nНадбавка " + nadbavka +
-//        "\nДо НДФЛ " + ((hours*stavka)*1.3f+nadbavka) +
-//        "\nЧистыми " + ((hours*stavka)*1.3f+nadbavka)*0.87f);
-
-//    public void onClick(View view) {
-//        Intent intent = new Intent();
-//        int hour = Integer.parseInt(editHours.getText().toString());
-//        float stavka = Float.parseFloat(editStavka.getText().toString());
-//        int Nadbavka = Integer.parseInt(editPNadbavka.getText().toString());
-//        intent.putExtra("hours", hour);
-//        intent.putExtra("stavka", stavka);
-//        intent.putExtra("nadbavka", Nadbavka);
-//        setResult(RESULT_OK, intent);
-//        finish();
-//    }
--- app/src/main/java/com/u/e/m/salary/TotalActivity.java
+++ app/src/main/java/com/u/e/m/salary/TotalActivity.java
@@ -2,14 +2,48 @@ package com.u.e.m.salary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TotalActivity extends AppCompatActivity {
+
+    TextView totalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_total);
         setTitle("Итог");
+
+        totalTextView = findViewById(R.id.totalTextView);
+
+        Intent totalIntent = getIntent();
+        String hours = totalIntent.getStringExtra("days");
+        String hoursMore = totalIntent.getStringExtra("daysMore");
+        String allowance = totalIntent.getStringExtra("daysAllovance");
+        String prize = totalIntent.getStringExtra("daysPrize");
+
+        float oneHours = 132.28f;
+        int hoursInt = Integer.parseInt(hours) * 8;// часов всего
+        int hoursMoreInt = Integer.parseInt(hoursMore) * 8; //часов переработки
+        int allowanceInt = Integer.parseInt(allowance); //персональная надбавка
+        int prizeInt = Integer.parseInt(prize); //разовая премия
+
+        totalTextView.setText("Оклад " + (hoursInt * oneHours) +
+                "\nНадбавка " + allowanceInt +
+                "\nПремия " + ((hoursMoreInt + hoursInt) * oneHours) * 0.3f +
+                "\nРазовая премия " + prizeInt +
+                "\nПереработано " + (hoursMoreInt * 2) * oneHours +
+                "\nДо вычета НДФЛ " + ((((hoursInt + (hoursMoreInt * 2)) * oneHours) + allowanceInt + prizeInt) +
+                (((hoursMoreInt + hoursInt) * oneHours) * 0.3f)) +
+                "\nПосле вычета НДФЛ " + (((((hoursInt + (hoursMoreInt * 2)) * oneHours) + allowanceInt + prizeInt) +
+                ((((hoursMoreInt + hoursInt) * oneHours) * 0.3f))) * 0.87f));
     }
 }
+
+//        ("Оклад " + (hours*stavka)  +
+//        "\nПремия " + (hours*stavka)*0.3f +
+//        "\nНадбавка " + nadbavka +
+//        "\nДо НДФЛ " + ((hours*stavka)*1.3f+nadbavka) +
+//        "\nЧистыми " + ((hours*stavka)*1.3f+nadbavka)*0.87f);

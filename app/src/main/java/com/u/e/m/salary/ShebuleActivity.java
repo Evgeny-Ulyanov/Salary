3a8aa1f271c475ecf7ad7400b22a55d3c7299183
Евгений <ulyanov.1992@list.ru>
2020-08-19 12:26:18 +0300

    Для кнопок сменный график и смешанный график добавлены Toast сообщения.

--- app/src/main/java/com/u/e/m/salary/ShebuleActivity.java
+++ app/src/main/java/com/u/e/m/salary/ShebuleActivity.java
@@ -1,17 +1,26 @@
 package com.u.e.m.salary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import static com.u.e.m.salary.R.drawable.logo_splash_screen;
 
 public class ShebuleActivity extends AppCompatActivity {
+    private ConstraintLayout mConstraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shebule);
+        mConstraintLayout =
+                (ConstraintLayout)findViewById(R.id.constraintLayout);
+        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
 
     }
 
@@ -20,9 +29,11 @@ public class ShebuleActivity extends AppCompatActivity {
     }
 
     public void changeShebule(View view) {
+        Toast.makeText(this, "Поведение кнопки не реализовано, всвязи с незначительными отличиями от рассчета по дневному графику", Toast.LENGTH_LONG).show();
     }
 
     public void mixShebule(View view) {
+        Toast.makeText(this, "Поведение кнопки не реализовано, всвязи с незначительными отличиями от рассчета по дневному графику", Toast.LENGTH_LONG).show();
     }
 
     public void chempack(View view) {
